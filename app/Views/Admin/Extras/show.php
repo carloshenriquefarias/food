4e75b321fa2265fba97a49aaf8290ed3b0531c2b
Carlos Henrique <carloshenriquepvh@hotmail.com>
2024-06-27 21:30:13 -0400

    feat:food-11 construindo tabelas controlers e models dos extras

---
+++ app/Views/Admin/Extras/show.php
@@ -0,0 +1,99 @@
+<?php echo $this->extend('Admin/layout/principal'); ?>
+
+<?php echo $this->section('titulo'); ?>
+  <?php echo $titulo; ?>
+<?php echo $this->endSection(); ?>
+
+
+<?php echo $this->section('estilos'); ?>
+
+<?php echo $this->endSection(); ?>
+
+<?php echo $this->section('conteudo'); ?>
+    <div class="row">
+        <div class="col-lg-6 grid-margin stretch-card">
+            <div class="card">
+                <div class="card-body">
+                    <h4 class="card-title"><?php echo esc($titulo)?></h4>                    
+                    <h4 class="card-title"><?php echo esc($extra->nome)?></h4>
+                    
+                    <p class="card-text">
+                        <strong>Nome:</strong> <?php echo esc($extra->nome)?>                        
+                    </p>
+
+                    <p class="card-text">
+                        <strong>Slug:</strong> <?php echo esc($extra->slug)?>                        
+                    </p>
+
+                    <p class="card-text">
+                        <strong>Ativo:</strong> <?php echo ($extra->ativo ? 'Sim' : 'Não')?>                        
+                    </p>
+
+                    <!-- O metodo humanize deixa no formato: criado ha '1 semana atras' -->
+                    <p class="card-text">
+                        <strong>Criado:</strong> <?php echo $extra->criado_em->humanize()?> 
+                    </p>
+
+                    <?php if ($extra->deletado_em === null): ?>
+                        <p class="card-text">
+                            <strong>Atualizado:</strong> <?php echo $extra->atualizado_em->humanize(); ?>
+                        </p>
+                    <?php else: ?>
+                        <p class="card-text text-danger">
+                            <strong>Excluído:</strong> <?php echo $extra->deletado_em->humanize(); ?>
+                        </p>
+                    <?php endif; ?>              
+                    
+                </div>
+
+                <div class="card-footer bg-primary">
+
+                    <?php if ($extra->deletado_em === null): ?>
+                        <a 
+                            href="<?php echo site_url('admin/extras/editar/'.$extra->id); ?>" 
+                            class="btn btn-dark btn-sm"
+                        >
+                            <i class="mdi mdi-pencil btn-icon-prepend"></i>
+                            Editar
+                        </a>    
+                        
+                        <a 
+                            href="<?php echo site_url('admin/extras/modalExcluir/'.$extra->id); ?>" 
+                            class="btn btn-danger btn-sm"
+                        >
+                            <i class="mdi mdi-trash-can btn-icon-prepend"></i>
+                            Excluir
+                        </a> 
+
+                        <a 
+                        href="<?php echo site_url('admin/extras/'); ?>" 
+                        class="btn btn-light btn-sm"
+                        >
+                            <i class="mdi mdi-arrow-left btn-icon-prepend"></i>
+                            Voltar
+                        </a> 
+                    <?php else: ?>
+
+                        <a data-toogle="tooltip" data-placement="top" title="Desfazer a exclusão" href="<?php echo site_url('admin/extras/desfazerexclusao/' . $extra->id); ?>" class="btn btn-dark btn-sm" style="text-decoration: none;">
+                            <i class="mdi mdi-undo btn-icon-prepend"></i>
+                            Recuperar
+                        </a>
+
+                        <a 
+                        href="<?php echo site_url('admin/extras/'); ?>" 
+                        class="btn btn-light btn-sm"
+                        >
+                            <i class="mdi mdi-arrow-left btn-icon-prepend"></i>
+                            Voltar
+                        </a> 
+                    <?php endif; ?> 
+                </div>
+            </div>
+        </div>
+    </div>
+<?php echo $this->endSection(); ?>
+
+<?php echo $this->section('scripts'); ?>
+  <!-- Aqui enviamos para o template principal os scripts -->
+
+<?php echo $this->endSection(); ?>
--- app/Views/Admin/layout/principal.php
+++ app/Views/Admin/layout/principal.php
@@ -185,6 +185,13 @@
           </li>
 
           <li class="nav-item">
+            <a class="nav-link" href="<?php echo site_url('admin/extras')?>">
+              <i class="mdi mdi-box-shadow menu-icon"></i>
+              <span class="menu-title">Extras</span>
+            </a>
+          </li>
+
+          <li class="nav-item">
             <a class="nav-link" href="<?php echo site_url('admin/usuarios')?>">
               <i class="mdi mdi-account-plus menu-icon"></i>
               <span class="menu-title">Usuarios</span>
